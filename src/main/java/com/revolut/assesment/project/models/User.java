93b0cb01531c2af266baa9fd09a36d25bbb3ca78
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2019-06-23 11:10:02 +0530

    Adding user model with builder mode and tests

---
+++ src/main/java/com/revolut/assesment/project/models/User.java
@@ -0,0 +1,21 @@
+package com.revolut.assesment.project.models;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Builder
+@Data
+@ToString
+public class User {
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String phoneNumber;
+    private String email;
+    private String gender;
+    private Date dateOfBirth;
+    private Date timeCreated;
+}
---
+++ src/test/java/com/revolut/assesment/project/models/UserTest.java
@@ -0,0 +1,23 @@
+package com.revolut.assesment.project.models;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void userBuilderTest() {
+        String city1 = "City1";
+        String fName1 = "FName1";
+        String lName1 = "lName1";
+        String email = "email@gmai.com";
+        User user = User.builder().city(city1).firstName(fName1).lastName(lName1).email(email).build();
+        Assert.assertNotNull(user);
+        Assert.assertEquals(city1, user.getCity());
+        Assert.assertEquals(fName1, user.getFirstName());
+        Assert.assertEquals(lName1, user.getLastName());
+        Assert.assertEquals(email, user.getEmail());
+    }
+}
