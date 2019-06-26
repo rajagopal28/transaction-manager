bcab036ac815ed321307a7d74d0e8a29b71b4250
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2019-06-26 11:14:11 +0530

    Fixing build failures due to change in models with relationships

--- src/test/java/com/revolut/assesment/project/model/AccountTest.java
+++ src/test/java/com/revolut/assesment/project/model/AccountTest.java
@@ -2,6 +2,7 @@ package com.revolut.assesment.project.model;
 
 import com.revolut.assesment.project.constants.ApplicationConstants;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.Date;
 
@@ -13,18 +14,19 @@ public class AccountTest {
         double balance = 100.00;
         String curr = "USD";
         Date timeCreated = new Date();
+        User mockUser = Mockito.mock(User.class);
         Account account = Account.builder()
                 .accountType(ApplicationConstants.AccountType.CREDIT)
                 .balance(balance).currency(curr)
                 .timeCreated(timeCreated)
                 .id(100)
-                .userId(121)
+                .user(mockUser)
                 .build();
         assertNotNull(account);
         assertEquals(ApplicationConstants.AccountType.CREDIT, account.getAccountType());
         assertEquals(curr, account.getCurrency());
         assertEquals(100, account.getId().intValue());
-        assertEquals(121, account.getUserId().intValue());
+        assertEquals(mockUser, account.getUser());
         assertEquals(timeCreated, account.getTimeCreated());
         assertEquals(balance, account.getBalance(), 0.001);
     }
--- src/test/java/com/revolut/assesment/project/model/TransactionTest.java
+++ src/test/java/com/revolut/assesment/project/model/TransactionTest.java
@@ -1,6 +1,7 @@
 package com.revolut.assesment.project.model;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.Date;
 
@@ -12,9 +13,11 @@ public class TransactionTest {
     public void testTransactionBuilder() {
         Date timeCreated = new Date();
         String currency = "INR";
+        Account fromAccount = Mockito.mock(Account.class);
+        Account toAccount = Mockito.mock(Account.class);
         Transaction transaction = Transaction.builder()
-                .fromAccount(101)
-                .toAccount(202)
+                .fromAccount(fromAccount)
+                .toAccount(toAccount)
                 .amount(23.00)
                 .currency(currency)
                 .timeCreated(timeCreated)
@@ -22,8 +25,8 @@ public class TransactionTest {
                 .build();
         assertNotNull(transaction);
         assertEquals(100, transaction.getId().intValue());
-        assertEquals(101, transaction.getFromAccount().intValue());
-        assertEquals(202, transaction.getToAccount().intValue());
+        assertEquals(fromAccount, transaction.getFromAccount());
+        assertEquals(toAccount, transaction.getToAccount());
         assertEquals(timeCreated, transaction.getTimeCreated());
         assertEquals(currency, transaction.getCurrency());
         assertEquals(23.00, transaction.getAmount(), 0.001);
